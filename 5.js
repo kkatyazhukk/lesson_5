1778c4b4daf8ba619167c4d8bd14cd77af0797e2
Ekaterina Zhuk <kkatyazhukk@gmail.com>
2022-02-13 19:44:00 +0100

    5th

--- 5.js
+++ 5.js
@@ -3,32 +3,32 @@
 //  Если суммы равны то ничья
 
 function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 let finalScore1 = 0;
 
 for (let i=0; i<3; i++) {
-    let attemptScore1 = getRandomInRange(1, 6);
-    console.log(attemptScore1);
-    finalScore1 += attemptScore1;
+  let attemptScore1 = getRandomInRange(1, 6);
+  console.log(attemptScore1);
+  finalScore1 += attemptScore1;
 }
 console.log(`The first player score = ${finalScore1}`);
 
-  
+
 let finalScore2 = 0;
 
 for (let i=0; i<3; i++) {
-    let attemptScore2 = getRandomInRange(1, 6);
-    console.log(attemptScore2);
-    finalScore2 += attemptScore2;
+  let attemptScore2 = getRandomInRange(1, 6);
+  console.log(attemptScore2);
+  finalScore2 += attemptScore2;
 }
 console.log(`The second player score = ${finalScore2}`);
 
 if (finalScore1>finalScore2) {
-    console.log(`The winner is the first player. His score is ${finalScore1}`);
+  console.log(`The winner is the first player. His score is ${finalScore1}`);
 } else if (finalScore1<finalScore2) {
-    console.log(`The winner is the second player. His score is ${finalScore2}`);
+  console.log(`The winner is the second player. His score is ${finalScore2}`);
 } else {console.log(`Draw - Friendship won!`);}
 
 
@@ -54,12 +54,12 @@ console.log(delta);
 let counter = 0;
 
 while (timestamp2>timestamp1) {
-    timestamp1 += 100000000;
-    let comparedDate = new Date(timestamp1); // convert delta to the date in UTC
-    if (comparedDate.getDay() === 5 && comparedDate.getDate() == 13) {
-        console.log(comparedDate);
-        counter++;
-    }
+  timestamp1 += 100000000;
+  let comparedDate = new Date(timestamp1); // convert delta to the date in UTC
+  if (comparedDate.getDay() === 5 && comparedDate.getDate() == 13) {
+      console.log(comparedDate);
+      counter++;
+  }
 }
 
 console.log(`There were ${counter} Fridays the 13th since 01/01/2020`);
@@ -72,23 +72,23 @@ console.log(`There were ${counter} Fridays the 13th since 01/01/2020`);
 // изначальное число целое, числа разбивки - целые (4,6,5)
 
 function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-  
-  let sum = 15; 
-  let partsNumber = 3;
-  
-  let limit = sum;
-  
-  for (let i=0; i<partsNumber; i++) {
-    if (i===partsNumber-1) {
-      let number = limit;
-      console.log (number);
-    } else {
-    let number = getRandomInRange(1,limit)
-    limit = limit - number;
-    console.log (number);}
-  }
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let sum = 15; 
+let partsNumber = 3;
+
+let limit = sum;
+
+for (let i=0; i<partsNumber; i++) {
+  if (i===partsNumber-1) {
+    let number = limit;
+    console.log (number);
+  } else {
+  let number = getRandomInRange(1,limit)
+  limit = limit - number;
+  console.log (number);}
+}
 
 
 // Задача 3.2
@@ -97,21 +97,21 @@ function getRandomInRange(min, max) {
 // числа разбивки дробные с 2 знаками после запятой (4.55, 5.20, 5.25)
 
 function getRandomFloat(min, max) {
-    let generatedNumber = Math.random() * (max - min) + min;
-    return generatedNumber.toFixed(2);
-  }
-  
-  let sum = 15; 
-  let partsNumber = 3;
-  
-  let limit = sum;
-  
-  for (let i=0; i<partsNumber; i++) {
-    if (i===partsNumber-1) {
-      let number = limit.toFixed(2);
-      console.log (number);
-    } else {
-    let number = getRandomFloat(1,limit-0.1)
-    limit = limit - number;
-    console.log (number);}
-  }
+  let generatedNumber = Math.random() * (max - min) + min;
+  return generatedNumber.toFixed(2);
+}
+
+let sum = 15; 
+let partsNumber = 3;
+
+let limit = sum;
+
+for (let i=0; i<partsNumber; i++) {
+  if (i===partsNumber-1) {
+    let number = limit.toFixed(2);
+    console.log (number);
+  } else {
+  let number = getRandomFloat(1,limit-0.1)
+  limit = limit - number;
+  console.log (number);}
+}
